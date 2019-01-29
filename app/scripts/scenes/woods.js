0e66513195130e7b23b00832cb01b0cd64d0faba
Kelly Stern <kmstern4@gmail.com>
2019-01-29 15:43:57 -0700

    added dialogue boxes and walking/idling anims to player and zombie

--- app/scripts/scenes/woods.js
+++ app/scripts/scenes/woods.js
@@ -1,3 +1,12 @@
+let i = 1;
+let text;
+let dialogue;
+let container;
+let hoodgirl;
+let farmzombie;
+let hgWalkOn;
+let fzWalkOn;
+
 export default class Woods extends Phaser.Scene {
   /**
    *  My custom scene.
@@ -40,6 +49,121 @@ export default class Woods extends Phaser.Scene {
     
     this.add.image(x, y, 'woods');
 
+    hoodgirl = this.add.sprite(-150, 400, 'hoodgirl', 'idle001.png');
+    farmzombie = this.add.sprite(800, 400, 'farmzombie', 'idle001.png');
+
+    dialogue = this.cache.json.get('dialogue');
+
+    let styledbox = this.add.image(0, 0, 'styledbox');
+
+
+    // let text = this.add.text(x, y, "TESTING PLS");
+    text = this.add.text(x, 150, dialogue.letter[0], {
+      wordWrap: { width: 390 }
+    });
+    text.setOrigin(0.5, 0.5);
+    text.setDepth(1);
+
+
+    container = this.add.container(x, 150, styledbox);
+    container.setSize(400, 100);
+
+
+
+    // TWEENS
+
+    // tween to make player walk in to scene
+    hgWalkOn = this.tweens.add({
+      targets: hoodgirl,
+      x: 150,
+      ease: 'power1',
+      duration: 2500,
+      repeat: 0,
+      paused: true
+    });
+
+    // zombie walks in to scene
+    fzWalkOn = this.tweens.add({
+      targets: farmzombie,
+      x: 500,
+      ease: 'power1',
+      duration: 2500,
+      repeat: 0,
+      paused: true
+    });
+ 
+
+
+
+    // DEFINING ANIMATIONS
+
+    // Hoodgirl animations:
+
+    // Hoodgirl idle infinite loop
+    this.anims.create({
+      key: 'hgidle',
+      frames: this.anims.generateFrameNames('hoodgirl', {
+        prefix: 'idle00',
+        suffix: '.png',
+        start: 1,
+        end: 18
+      }),
+      frameRate: 15,
+      repeat: -1
+    });
+
+    // Hoodgirl walking loops twice
+    this.anims.create({
+      key: 'hgwalking',
+      frames: this.anims.generateFrameNames('hoodgirl', {
+        prefix: 'walking00',
+        suffix: '.png',
+        start: 1,
+        end: 24
+      }),
+      frameRate: 20,
+      repeat: 1
+    });
+
+
+    // Farmzombie animations:
+
+    // farmzombie idle infinite loop
+    this.anims.create({
+      key: 'fzidle',
+      frames: this.anims.generateFrameNames('farmzombie', {
+        prefix: 'idle00',
+        suffix: '.png',
+        start: 1,
+        end: 12
+      }),
+      frameRate: 10,
+      repeat: -1
+    });
+
+    // farmzombie walking loops twice
+    this.anims.create({
+      key: 'fzwalking',
+      frames: this.anims.generateFrameNames('farmzombie', {
+        prefix: 'walking00',
+        suffix: '.png',
+        start: 1,
+        end: 18
+      }),
+      frameRate: 20,
+      repeat: 2
+    });
+
+
+    // CALLING ANIMATIONS
+    hoodgirl.on('animationcomplete', function() {
+      hoodgirl.play('hgidle');
+    });
+
+    farmzombie.on('animationcomplete', function() {
+      farmzombie.play('fzidle');
+    });
+
   }
 
   /**
@@ -50,6 +174,21 @@ export default class Woods extends Phaser.Scene {
    *  @param {number} dt - Time elapsed since last update.
    */
   update(/* t, dt */) {
+    const keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+    if (Phaser.Input.Keyboard.JustDown(keySpace)) {
+      if (dialogue.letter[i] !== undefined) {
+        text.setText(dialogue.letter[i]);
+        i++;
+      } else {
+        container.visible = false;
+        text.visible = false;
+        farmzombie.anims.play('fzwalking', true);
+        fzWalkOn.restart();
+        hoodgirl.anims.play('hgwalking', true);
+        hgWalkOn.restart();
+      }
+    }
   }
 
   /**
