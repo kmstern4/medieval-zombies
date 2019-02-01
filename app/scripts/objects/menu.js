1ececa0d6906a34341aefe88d85a67c799e6b264
Kelly Stern <kmstern4@gmail.com>
2019-02-01 15:21:07 -0700

    attack menu is separated into game objects

--- app/scripts/objects/menu.js
+++ app/scripts/objects/menu.js
@@ -12,14 +12,57 @@ export default class Menu extends Phaser.GameObjects.Container {
    *  @param {number} y - The vertical coordinate relative to the scene viewport.
    */
   constructor(scene, x, y) {
-    super(scene, x, y, 'menu');
+    super(scene, x, y);
     this.menuItems = [];
-    this.menuItemIndex = 0;
+    this.index = 0;
     this.x = x;
     this.y = y;
-
+    this.addMenuItem('Attack');
+    this.addMenuItem('Special Attack');
+    this.addMenuItem('Potion');  
     //  Add this game object to the owner scene.
     scene.children.add(this);
+  }
+
+  addMenuItem(unit) {
+    var menuItem = new MenuItem(this.scene, 0, this.menuItems.length * 20, unit);
+    this.menuItems.push(menuItem);
+    this.add(menuItem);
+  }
+
+  moveSelectionUp() {
+    this.menuItems[this.index].deselect();
+    this.index--;
+    if (this.index < 0) {
+      this.index = this.menuItems.length - 1;
+    }
+    this.menuItems[this.index].select();
+  }
+
+  moveSelectionDown() {
+    this.menuItems[this.index].deselect();
+    this.index++;
+    if (this.index >= this.menuItems.length) {
+      this.index = 0;
+    }
+    this.menuItems[this.index].select();
+  }
+
+  confirm() {
+    switch(this.index) {
+    case 0:
+      console.log('Attack');
+      break;
+    case 1: 
+      console.log('Defend');
+      break;
+    case 2:
+      console.log('Potion');
+      break;
+    default:
+      console.log(`You somehow hit a wrong index of ${this.index}`);
+      break;
+    }
   }
 
   
--- app/scripts/scenes/title.js
+++ app/scripts/scenes/title.js
@@ -116,7 +116,7 @@ export default class Title extends Phaser.Scene {
       }),
       repeat: -1,
       frameRate: 15
-    })
+    });
 
     hoodgirl.play('hgwalking');
     hoodgirl.on('animationcomplete', function() {
@@ -148,7 +148,7 @@ export default class Title extends Phaser.Scene {
       strokeThickness: 6
     });
 
-    const townLabel = this.add.text(x, 450, 'Skip to Town', {
+    const townLabel = this.add.text(x, 450, 'Town Scene', {
       font: '20px Lucida Console',
       color: 'white',
       stroke: 'black',
@@ -163,7 +163,7 @@ export default class Title extends Phaser.Scene {
     hoodboy.on('pointerup', () => {
       this.rhythmloop.stop();
       this.scene.start('Woods', { char: 'hoodboy' });
-    })
+    });
 
     label2.setOrigin(0.5, 0.5);
 
@@ -172,7 +172,7 @@ export default class Title extends Phaser.Scene {
     townLabel.on('pointerup', () => {
       this.rhythmloop.stop();
       this.scene.start('Town');
-    })
+    });
     
   }
 
