2d099846634b4bb36139fe8aab8e86e764d3e3ba
Kelly Stern <kmstern4@gmail.com>
2019-04-12 14:17:23 -0600

    updated instance configuration

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,16 +1,7 @@
-/*
- *  `app` module
- *  ============
- *
- *  Provides the game initialization routine.
- */
-
 //  Import game instance configuration.
 import * as config from '@/config';
 
-/**
- *  Create a `Phaser.Game` instance and boot the game.
- */
+// Create a `Phaser.Game` instance and boot the game
 export function boot() {
   return new Phaser.Game(config);
 }
